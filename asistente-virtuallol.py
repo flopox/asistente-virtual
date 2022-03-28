763a197dbb592a07ee06069699c6a2ee32dbf84a
flopox <l.llop@alumno.etec.um.edu.ar>
2022-03-28 10:01:25 -0300

    .

--- asistente-virtuallol.py
+++ asistente-virtuallol.py
@@ -11,3 +11,31 @@ voices =  engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 for i in voices :
     print (i)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def listen(): 
+    try: 
+        with sr.Microphone() as source: 
+            print ("Escuchando...")
+            pc = Listener.listen(source) 
+            rec = Listener.recognize_google(pc)
+            rec = rec.lower()
+            if name in rec:
+                rec = rec.replace(name, '')
+    except:
+        pass
+    return rec
+
+def run_maría():
+    rec = listen() 
+    if ' reproduce ' in rec: 
+        music = rec.replace('reproduce', '')
+        print ("reproduciendo..." + music)
+        talk ("reproduciendo..." + music)
+        pywhatkit.playonyt(music)
+if __name__ == '__main__': 
+    run_maría()
+    
