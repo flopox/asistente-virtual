85d3cccf39b04bdff099027e3849e0ad4e528952
flopox <l.llop@alumno.etec.um.edu.ar>
2022-04-04 12:03:16 -0300

    .

--- asistente.py
+++ asistente.py
@@ -1,6 +1,7 @@
-from traceback import StackSummary
+from importlib.metadata import files
 import speech_recognition as sr
-import pyttsx3, pywhatkit, wikipedia, datetime, keyboard
+import subprocess as sub
+import pyttsx3, pywhatkit, wikipedia, datetime, keyboard, colores, os
 from pygame import mixer 
 
 name = "emma"
@@ -9,6 +10,23 @@ engine = pyttsx3.init()
  
 voices =  engine.getProperty('voices') 
 engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 145)
+
+sites={
+            'google':'google.com',
+            'youtube':'youtube.com',
+            'facebook':'facebook.com',
+            'whatsapp':'whatsapp.com',
+            'twitter':'twitter.com',
+            'instagram':'instagram.com'
+
+        }
+files={
+            'mensaje':'Luca.Llop_Mensaje-Oculto.xlsx',
+            'unidades':'Luca-Llop_UNIDADES DE ALMACENAMIENTO-Tarea2.docx',
+            'evolucion':'SISTEMAS OPERATIVOS - TP N°2 - Evolución de versiones de SO -.docx'
+
+        }
 
 def talk(text):
     engine.say(text)
@@ -54,6 +72,38 @@ def run_emma():
                     if keyboard.read_key() == "s":
                         mixer.music.stop()
                         break
+        elif 'colores' in rec:
+            talk("Enseguida")
+            colores.capture()
+        elif 'abre' in rec:
+            for site in sites:
+                if site in rec:
+                    sub.call(f'start chrome.exe{sites[site]}', shell=True)
+                    talk(f'Abriendo {site}')
+        elif 'archivo' in rec:
+            for file in files:
+                if file in rec:
+                    sub.Popen([files[file]], shell=True)
+                    talk(f'abriendo {file}')
+        elif 'escribe' in rec:
+            try:
+                with open("mensaje.xlsx", 'a') as f:
+                    write(f)
+            except FileNotFoundError as e:
+                file = open("mensaje.xlsx", 'w')
+                write(file)
+        elif 'termina' in rec:
+            talk('Adios!')
+            break
+def write(f):
+    talk("¿Que quieres que escriba?")
+    rec_write = listen()
+    f.write(rec_write + os.linesep)
+    f.close()
+    talk("Listo, puedes revisarlo")
+    sub.Popen("nota.txt", shell=True)
+
+
 
 if __name__ == '__main__': 
     run_emma()
